165bacd5f72e2d56fc06fe35b718fb4768fa3c72
ajay776 <vishwakarmaajay776@gmail.com>
2024-04-17 20:23:24 +0530

    added traverse linkedlist

--- DSAlgoPlayground/LinkedList/crud.py
+++ DSAlgoPlayground/LinkedList/crud.py
@@ -5,21 +5,37 @@ class Node:
 
 
 class LinkedList:
-    def _init_(self, node):
-        pass
+
+    def __init__(self, node):
+        self.node =  node 
+        self.current_node = self.node
+        self.result_list = [] 
 
     def traverseList(self):
-        pass
-
+        while self.current_node:
+            self.result_list.append(self.current_node.val)
+            self.current_node = self.current_node.next
+        return self.result_list
+        
+        
     def insertElementAtEnd(self, node):
         pass
 
 
 a = Node(2)
+b = Node(4)
+c = Node(10)
+d = Node(6)
+e = Node(7)
+a.next = b
+b.next = c
+c.next = d
+d.next = e
 
 window_traverser = LinkedList(a)
-window_traverser.insertElementAtEnd(Node(4))
-window_traverser.insertElementAtEnd(Node(6))
-window_traverser.insertElementAtEnd(Node(0))
-
 print(window_traverser.traverseList())
+# window_traverser.insertElementAtEnd(Node(4))
+# window_traverser.insertElementAtEnd(Node(6))
+# window_traverser.insertElementAtEnd(Node(0))
+
+# print(window_traverser.traverseList())
